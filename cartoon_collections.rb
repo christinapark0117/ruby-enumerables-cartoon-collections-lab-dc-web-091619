f38da30352463c9eeaabcfbe5de1cd459af9d874
Christina Park <christinapark0117@gmail.com>
2019-09-12 01:40:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |cheese|
+  cheese.find do |cheese|
     cheese_types.include? (cheese)
  end
 end
