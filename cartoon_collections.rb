a33e25c2ebb6f2af91e69a79fe9e6e216be60bd8
Christina Park <christinapark0117@gmail.com>
2019-09-12 01:28:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(array)
 end
 end
 
-def find_the_cheese(cheese)
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
     cheese_types.include? (cheese)
