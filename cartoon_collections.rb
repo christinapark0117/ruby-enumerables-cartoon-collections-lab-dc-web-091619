d97ec98441f962c7aae146afa715ca1641bc6225
Christina Park <christinapark0117@gmail.com>
2019-09-12 01:37:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planteer_calls)
-  planteer_calls.collect! 
+  planteer_calls.collect! {|call| call.capitalize}
 end
 
 def long_planeteer_calls(calls)
